5afa60a2614ae02020d07b4faae0569292047481
sz <stefan.zindl@outlook.de>
2025-02-20 13:20:13 +0100

    update to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://zindlsn.github.io',
+	base: 'zindl_hp',
 	integrations: [mdx(), sitemap()],
 });
