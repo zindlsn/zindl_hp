5a0fe03882c00e2696a3144e0f1aa840af887b3e
sz <stefan.zindl@outlook.de>
2025-02-22 09:46:29 +0100

    Adds github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://zindlsn.github.io',
 	base:'/zindl_hp',
+	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
