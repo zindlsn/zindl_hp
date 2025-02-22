bb9be9ec70a29a5196ef712544123ec57116037c
sz <stefan.zindl@outlook.de>
2025-02-22 11:53:20 +0100

    update live url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,25 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+const SERVER_PORT = 3000;
+const LIVE_URL = `https://zindlsn.github.io/zindl_hp`;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const SCRIPT = process.env.npm_lifecyclescript ||"";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL
+let page_path = ""
+
+if(isBuild){
+	BASE_URL = LIVE_URL;
+	page_path = `zindlsn_hp`
+}else{
+	console.log(BASE_URL)
+}
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://zindlsn.github.io',
-	base:'/zindl_hp',
+	site: BASE_URL,
+	base: page_path,
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
